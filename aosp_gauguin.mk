a50b67d484c6bba3da1da50029c390f67965bc10
Droneship <81514881+Seifood@users.noreply.github.com>
2021-06-08 12:06:06 +0200

    Pixel blaster bringup

--- aosp_gauguin.mk
+++ aosp_gauguin.mk
@@ -7,8 +7,8 @@
 # Inherit from gauguin device
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/nezuko/config/common.mk)
+# Inherit some common pixel stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
@@ -17,13 +17,14 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := nezuko_gauguin
+PRODUCT_NAME := aosp_gauguin
 PRODUCT_DEVICE := gauguin
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-USE_GAPPS := true
+#Blaster
+BLASTER_BUILD_TYPE := OFFICIAL
 
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
 BUILD_DESCRIPTION := redfin-user 11 RQ2A.210505.003 7255357 release-keys
